05edab3982209698e898dfc4d7fabe71c1c02c97
Arlette3097 <arlette.sanchez@upaep.edu.mx>
2021-07-07 16:52:13 -0500

    mercado liebre terminado

---
+++ app.js
@@ -0,0 +1,24 @@
+const express=require ('express');
+const path=require('path');
+
+const app=express();
+app.use(express.static('public'));
+
+app.listen(process.env.PORT||3000,function(){
+    console.log('Servidor corriendo en el puerto 3000');
+});
+
+app.get('/home', (req,res) => {
+    let htmlPath = path.resolve(__dirname,'./views/home.html');
+    res.sendFile(htmlPath);
+} )
+
+app.get('/login', (req,res) => {
+    let htmlPath = path.resolve(__dirname,'./views/login.html');
+    res.sendFile(htmlPath);
+} )
+
+app.get('/signup', (req,res) => {
+    let htmlPath = path.resolve(__dirname,'./views/signup.html');
+    res.sendFile(htmlPath);
+} )
